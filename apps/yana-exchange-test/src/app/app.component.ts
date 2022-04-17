d354042a66f3b1af604580c38c47f8358564f2c1
Keyur Shah <keyurshah3939@gmail.com>
2022-04-18 00:10:56 +0530

    Test Created

---
+++ apps/yana-exchange-test/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'yana-exchange-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'yana-exchange-test';
+}
